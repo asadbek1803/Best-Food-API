586b8f299d1fcc2cf024b51109eeddea21783bbd
Asadbek <asadbekoffical1202@gmail.com>
2025-02-15 12:18:26 +0500

    Fix: Error updateting

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+import bot_urls
+import admin_urls
 
 urlpatterns = [
-    path('bot/', include('bot_urls')),
-    path('admin/', include('admin_urls'))
+    path('bot/', include(bot_urls)),
+    path('admin/', include(admin_urls))
 ]
