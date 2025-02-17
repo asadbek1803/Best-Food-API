aecd5589b5bde996a659e9c5cbaa0d11c0448259
Asadbek <asadbekoffical1202@gmail.com>
2025-02-17 11:36:11 +0500

    Update: Apis all update

--- api/serializers.py
+++ api/serializers.py
@@ -63,48 +63,23 @@ class CategoryForProduct(serializers.ModelSerializer):
         model = Category
         fields = ('name', )
 
-    def get_name(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')  # Default til - O‘zbekcha
-        return getattr(obj, f"{language}_name", obj.uz_name)  # Til bo‘yicha chiqarish
-
+    
 
 class CategorySerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-
+    
     class Meta:
         model = Category
         exclude = ['uz_name', 'ru_name', 'en_name', 'uz_description', 'ru_description', 'en_description']  
 
-    def get_name(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')
-        return getattr(obj, f"{language}_name", obj.uz_name)
-
-    def get_description(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')
-        return getattr(obj, f"{language}_description", obj.uz_description)
+    
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-
     class Meta:
         model = Product
         exclude = ['uz_name', 'ru_name', 'en_name', 'uz_description', 'ru_description', 'en_description']
 
-    def get_name(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')
-        return getattr(obj, f"{language}_name", obj.uz_name)
-
-    def get_description(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')
-        return getattr(obj, f"{language}_description", obj.uz_description)
+    
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -122,13 +97,10 @@ class DeliverySerializer(serializers.ModelSerializer):
 
 
 class RatingSerializer(serializers.ModelSerializer):
-    comment = serializers.SerializerMethodField()
+    
 
     class Meta:
         model = Rating
         exclude = ['uz_comment', 'ru_comment', 'en_comment']
 
-    def get_comment(self, obj):
-        request = self.context.get('request')
-        language = request.query_params.get('lang', 'uz')
-        return getattr(obj, f"{language}_comment", obj.uz_comment)
+   
